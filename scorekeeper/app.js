bfb31648968428a78b708eb59d659333b03594e4
wzqj <9105604+wzqj@users.noreply.github.com>
2022-09-15 06:39:48 -0700

    scorekeeper: fix bugs and refactor

--- scorekeeper/app.js
+++ scorekeeper/app.js
@@ -1,6 +1,5 @@
 function addPoint() {
     this.score += 1;
-    console.log(this);
     scoreboard.innerText = `${p1PointBtn.score} to ${p2PointBtn.score}`;
     if (p1PointBtn.score >= parseInt(maxScore) || p2PointBtn.score >= parseInt(maxScore)) {
         p1PointBtn.disabled = true;
@@ -19,26 +18,17 @@ const scoreboard = document.querySelector('h1');
 
 const p1PointBtn = document.querySelector('.player1');
 p1PointBtn.score = 0;
-p1PointBtn.addEventListener('click', function (e) {
-    p1Score += 1;
-    scoreboard.innerText = `${p1Score} to ${p2Score}`;
-    if (p1Score >= parseInt(maxScore) || p2Score >= parseInt(maxScore)) {
-        p1PointBtn.disabled = true;
-        p2PointBtn.disabled = true;
-    }
-});
+p1PointBtn.addEventListener('click', addPoint);
 
 const p2PointBtn = document.querySelector('.player2');
 p2PointBtn.score = 0;
-p2PointBtn.addEventListener('click', function (e) {
-    addPoint();
-});
+p2PointBtn.addEventListener('click', addPoint);
 
 const resetBtn = document.querySelector('.reset');
 resetBtn.addEventListener('click', function (e) {
-    p1Score = 0;
-    p2Score = 0;
-    scoreboard.innerText = `${p1Score} to ${p2Score}`;
+    p1PointBtn.score = 0;
+    p2PointBtn.score = 0;
+    scoreboard.innerText = `${p1PointBtn.score} to ${p2PointBtn.score}`;
     p1PointBtn.disabled = false;
     p2PointBtn.disabled = false;
 });
