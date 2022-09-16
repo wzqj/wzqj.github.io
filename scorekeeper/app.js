4d9e504595ff09b832dfd2ceda366597a74fcd3b
wzqj <9105604+wzqj@users.noreply.github.com>
2022-09-15 22:58:39 -0700

    scorekeeper: change scores to separate spans for coloured win/loss

--- scorekeeper/app.js
+++ scorekeeper/app.js
@@ -1,9 +1,18 @@
 function addPoint() {
     this.score += 1;
-    scoreboard.innerText = `${p1PointBtn.score} to ${p2PointBtn.score}`;
+    p1Score.innerText = `${p1PointBtn.score}`;
+    p2Score.innerText = `${p2PointBtn.score}`;
     if (p1PointBtn.score >= maxScore || p2PointBtn.score >= maxScore) {
         p1PointBtn.disabled = true;
         p2PointBtn.disabled = true;
+
+        if (p1PointBtn.score > p2PointBtn.score) {
+            p1Score.style.color = 'green';
+            p2Score.style.color = 'red';
+        } else {
+            p1Score.style.color = 'red';
+            p2Score.style.color = 'green';
+        }
     }
 }
 
@@ -14,7 +23,8 @@ select.addEventListener('change', (e) => {
     maxScore = parseInt(select.value);
 });
 
-const scoreboard = document.querySelector('h1');
+const p1Score = document.querySelector('.p1Score');
+const p2Score = document.querySelector('.p2Score');
 
 const p1PointBtn = document.querySelector('.player1');
 p1PointBtn.score = 0;
@@ -28,7 +38,8 @@ const resetBtn = document.querySelector('.reset');
 resetBtn.addEventListener('click', function (e) {
     p1PointBtn.score = 0;
     p2PointBtn.score = 0;
-    scoreboard.innerText = `${p1PointBtn.score} to ${p2PointBtn.score}`;
+    p1Score.innerText = `${p1PointBtn.score}`;
+    p2Score.innerText = `${p2PointBtn.score}`;
     p1PointBtn.disabled = false;
     p2PointBtn.disabled = false;
 });
